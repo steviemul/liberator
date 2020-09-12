e56667cf5b9c8d79405fbc411512394260834b35
steviemul <stephen.mulrennan@gmail.com>
2020-09-12 05:52:41 +0100

    Checkpoint

--- db/data.sql
+++ db/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO LIBRARY (NAME, DOWNLOAD_COUNT, AUTHOR) VALUES ('Piccadilly', 0, 'stephen mulrennan');
+USE LIBRARIES;
+
+INSERT INTO library (NAME, DOWNLOAD_COUNT, AUTHOR) VALUES ('Piccadilly', 0, 'stephen mulrennan');
+INSERT INTO library (NAME, DOWNLOAD_COUNT, AUTHOR) VALUES ('ORG.JUNIT', 30, 'stephen mulrennan');
 
